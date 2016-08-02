6eecfdfde6379eaee24bddeb38d94287fd5c3453
rende11 <artamonovn@gmail.com>
2016-08-02 08:10:48 +0300

    tab->spaces

--- tests/LinterTest.php
+++ tests/LinterTest.php
@@ -4,11 +4,10 @@ namespace PsrLinter;
 
 class LinterTest extends \PHPUnit_Framework_TestCase
 {
-	public function testGetData()
-	{
-		$data = 'data';
-		$linter = new Linter($data);
-
-		$this->assertEquals($data, $linter->getData());
-	}
+    public function testGetData()
+    {
+        $data = 'data';
+        $linter = new Linter($data);
+        $this->assertEquals($data, $linter->getData());
+    }
 }
